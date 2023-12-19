d9561ead23dd5420f1ec8c07a02427e10aa7fc58
Enuma <enumauzoegwu@yahoo.com>
2023-12-19 16:20:56 +0100

    boiler plate commit

---
+++ src/main/java/com/ostreach/OstreachApplication.java
@@ -0,0 +1,13 @@
+package com.ostreach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OstreachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OstreachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ostreach/OstreachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ostreach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OstreachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
